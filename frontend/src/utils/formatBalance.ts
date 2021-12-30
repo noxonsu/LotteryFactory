75098d5c5854a73d835eba2a0c74836a44081ee2
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-12-30 17:46:52 +0600

    fix claim reward number

--- frontend/src/utils/formatBalance.ts
+++ frontend/src/utils/formatBalance.ts
@@ -4,25 +4,28 @@ import { formatUnits } from 'ethers/lib/utils'
 import { getLanguageCodeFromLS } from 'contexts/Localization/helpers'
 import { BIG_TEN } from './bigNumber'
 
+// @ts-ignore
+const lotteryTokenDecimals = parseInt(window.SO_LotteryConfig.token.decimals, 10)
+
 /**
  * Take a formatted amount, e.g. 15 BNB and convert it to full decimal value, e.g. 15000000000000000
  */
-export const getDecimalAmount = (amount: BigNumber, decimals = 18) => {
+export const getDecimalAmount = (amount: BigNumber, decimals = lotteryTokenDecimals || 18) => {
   return new BigNumber(amount).times(BIG_TEN.pow(decimals))
 }
 
-export const getBalanceAmount = (amount: BigNumber, decimals = 18) => {
+export const getBalanceAmount = (amount: BigNumber, decimals = lotteryTokenDecimals || 18) => {
   return new BigNumber(amount).dividedBy(BIG_TEN.pow(decimals))
 }
 
 /**
  * This function is not really necessary but is used throughout the site.
  */
-export const getBalanceNumber = (balance: BigNumber, decimals = 18) => {
+export const getBalanceNumber = (balance: BigNumber, decimals = lotteryTokenDecimals || 18) => {
   return getBalanceAmount(balance, decimals).toNumber()
 }
 
-export const getFullDisplayBalance = (balance: BigNumber, decimals = 18, displayDecimals?: number) => {
+export const getFullDisplayBalance = (balance: BigNumber, decimals = lotteryTokenDecimals || 18, displayDecimals?: number) => {
   return getBalanceAmount(balance, decimals).toFixed(displayDecimals)
 }
 
@@ -38,7 +41,7 @@ export const formatNumber = (number: number, minPrecision = 2, maxPrecision = 2)
  * Method to format the display of wei given an ethers.BigNumber object
  * Note: does NOT round
  */
-export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
+export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = lotteryTokenDecimals || 18, decimals = lotteryTokenDecimals || 18) => {
   const remainder = number.mod(ethers.BigNumber.from(10).pow(decimals - displayDecimals))
   return formatUnits(number.sub(remainder), decimals)
 }
@@ -47,7 +50,7 @@ export const formatBigNumber = (number: ethers.BigNumber, displayDecimals = 18, 
  * Method to format the display of wei given an ethers.BigNumber object with toFixed
  * Note: rounds
  */
-export const formatBigNumberToFixed = (number: ethers.BigNumber, displayDecimals = 18, decimals = 18) => {
+export const formatBigNumberToFixed = (number: ethers.BigNumber, displayDecimals = lotteryTokenDecimals || 18, decimals = lotteryTokenDecimals || 18) => {
   const formattedString = formatUnits(number, decimals)
   return (+formattedString).toFixed(displayDecimals)
 }
@@ -56,7 +59,7 @@ export const formatBigNumberToFixed = (number: ethers.BigNumber, displayDecimals
  * Formats a FixedNumber like BigNumber
  * i.e. Formats 9763410526137450427.1196 into 9.763 (3 display decimals)
  */
-export const formatFixedNumber = (number: ethers.FixedNumber, displayDecimals = 18, decimals = 18) => {
+export const formatFixedNumber = (number: ethers.FixedNumber, displayDecimals = lotteryTokenDecimals || 18, decimals = lotteryTokenDecimals || 18) => {
   // Remove decimal
   const [leftSide] = number.toString().split('.')
   return formatBigNumber(ethers.BigNumber.from(leftSide), displayDecimals, decimals)
