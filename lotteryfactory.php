442cf525ece92d99ec11f402585e94302a65c47b
shendel <im_shendel@inbox.ru>
2022-01-08 16:31:45 +0300

    version up
    1.0.22

--- lotteryfactory.php
+++ lotteryfactory.php
@@ -5,7 +5,7 @@ Description: Blockchain Lottery
 Requires PHP: 7.1
 Text Domain: lotteryfactory
 Domain Path: /lang
-Version: 1.0.21
+Version: 1.0.22
  */
 
 /* Define Plugin Constants */
@@ -15,7 +15,7 @@ define( 'LOTTERYFACTORY_BASE_DIR', __DIR__ );
 define( 'LOTTERYFACTORY_BASE_FILE', __FILE__ );
 define( 'LOTTERYFACTORY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LOTTERYFACTORY_URL', plugin_dir_url( __FILE__ ) );
-define( 'LOTTERYFACTORY_VER', '1.0.21');
+define( 'LOTTERYFACTORY_VER', '1.0.22');
 
 /**
  * Plugin Init
