63502e9a96b2e77560b4bd5f846be30a1d49b5c0
noxPC <2914674+noxonsu@users.noreply.github.com>
2023-01-12 23:19:49 +0300

    new react bui,ld

--- lotteryfactory.php
+++ lotteryfactory.php
@@ -2,10 +2,10 @@
 /**
 Plugin Name: Lottery Factory
 Description: Blockchain Lottery
-Requires PHP: 7.1
+Requires PHP: 7.2
 Text Domain: lotteryfactory
 Domain Path: /lang
-Version: 1.1.1104
+Version: 1.1.1105
  */
 
 /* Define Plugin Constants */
@@ -15,7 +15,7 @@ define( 'LOTTERYFACTORY_BASE_DIR', __DIR__ );
 define( 'LOTTERYFACTORY_BASE_FILE', __FILE__ );
 define( 'LOTTERYFACTORY_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LOTTERYFACTORY_URL', plugin_dir_url( __FILE__ ) );
-define( 'LOTTERYFACTORY_VER', '1.1.1104');
+define( 'LOTTERYFACTORY_VER', '1.1.1105');
 
 /**
  * Plugin Init
--- scripts/rebuildReactSources.sh
+++ scripts/rebuildReactSources.sh
@@ -3,4 +3,4 @@ yarn install
 npm run build_clean
 cp -R build/* ../vendor_source
 npm version patch
-perl -i -pe 's/\b(\d+)(?=\D*$)/$1+1/e' lotteryfactory.php
+perl -i -pe 's/\b(\d+)(?=\D*$)/$1+1/e' ../lotteryfactory.php
