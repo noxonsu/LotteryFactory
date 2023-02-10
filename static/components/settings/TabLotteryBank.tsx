feb2a8f695bb5ff0a71f6a7dd802d65505e78d28
shendel <im_shendel@inbox.ru>
2023-02-10 20:08:11 +0300

    withdraw lottery bank tab

--- static/components/settings/TabLotteryBank.tsx
+++ static/components/settings/TabLotteryBank.tsx
@@ -7,6 +7,9 @@ import FaIcon from "../FaIcon"
 import callLotteryMethod from "../../helpers/callLotteryMethod"
 import SwitchNetworkAndCall from "../SwitchNetworkAndCall"
 import fetchLotteryStatus from "../../helpers/fetchLotteryStatus"
+import fetchTokenBalance from "../../helpers/fetchTokenBalance"
+import { getDateDiffText } from "../../helpers/getDateDiffText"
+import { getUnixTimestamp } from "../../helpers/getUnixTimestamp"
 
 export default function TabLotteryBank(options) {
   const {
@@ -20,17 +23,55 @@ export default function TabLotteryBank(options) {
   } = options
   
   const [ isBankWithdraw, setIsBankWithdraw ] = useState(false)
+  
+  const [ endTime, setEndTime ] = useState(0)
+  const [ lotteryStatus, setLotteryStatus ] = useState(0)
+  const [ balanceInfo, setBalanceInfo ] = useState(false)
+  const [ timeLeft, setTimeLeft ] = useState(0)
+  const [ doFetchStatus, setDoFetchStatus ] = useState(true)
+  
   const doBankWithdraw = () => {
+    openConfirmWindow({
+      title: `Withdraw Lottery bank`,
+      message: `Do you really want to withdraw the entire bank from the lottery contract?`,
+      onConfirm: () => {
+        setIsBankWithdraw(true)
+        const { activeWeb3 } = getActiveChain()
+        callLotteryMethod({
+          activeWeb3,
+          contractAddress: storageData.lotteryAddress,
+          method: 'withdrawBank',
+          args: [
+            balanceInfo.wei
+          ],
+        }).then((res) => {
+          setIsBankWithdraw(false)
+          doFetchStatus(true)
+          addNotify(`Lottery bank withdrawed`, `success`)
+        }).catch((err) => {
+          setIsBankWithdraw(false)
+          addNotify(`Fail withdraw lottery bank. ${err.message ? err.message : ''}`, `error`)
+        })
+      }
+    })
   }
   
   useEffect(() => {
-    if (storageData.chainId && storageData.lotteryAddress) {
+    if (storageData.chainId && storageData.lotteryAddress && storageData.tokenAddress && doFetchStatus) {
       addNotify(`Fetching lottery status`)
       fetchLotteryStatus({
         chainId: storageData.chainId,
         contractAddress: storageData.lotteryAddress,
       }).then((lotteryStatus) => {
-        console.log('>>> BANK Lottery status', lotteryStatus)
+        const { currentLotteryInfo } = lotteryStatus
+        setEndTime(currentLotteryInfo.endTime)
+        setLotteryStatus(currentLotteryInfo.status)
+        setTimeLeft(Number(currentLotteryInfo.endTime) + 31 * 24 * 60 * 60 - getUnixTimestamp())
+        // fetch bank
+        fetchTokenBalance(storageData.lotteryAddress, storageData.tokenAddress, storageData.chainId).then((balanceInfo) => {
+          setBalanceInfo(balanceInfo)
+          setDoFetchStatus(false)
+        })
       })
     }
   }, [ storageData ])
@@ -44,56 +85,66 @@ export default function TabLotteryBank(options) {
             </div>
             <div className={styles.subFormInfo}>
               <h3>Lottery bank info</h3>
-              <div className={styles.subForm}>
-                <div className={styles.infoRow}>
-                  <label>
-                    <div className={styles.helpTooltip}>
-                      <span>?</span>
-                      <div>{`Date when the last round was closed`}</div>
+              {(storageData.tokenInfo && storageData.chainId && storageData.lotteryAddress && storageData.tokenInfo.symbol && balanceInfo) ? (
+                <div className={styles.subForm}>
+                  <div className={styles.infoRow}>
+                    <label>
+                      <div className={styles.helpTooltip}>
+                        <span>?</span>
+                        <div>{`Date when the last round was closed`}</div>
+                      </div>
+                      Last round ends in:
+                    </label>
+                    <div>
+                      <strong>{new Date(Number(endTime) * 1000).toString()}</strong>
                     </div>
-                    Last round ends in:
-                  </label>
-                  <div>
-                    Time
+                  </div>
+                  <div className={styles.infoRow}>
+                    <label>
+                      <div className={styles.helpTooltip}>
+                        <span>?</span>
+                        <div>{`How much time has passed since the last round closed`}</div>
+                      </div>
+                      Time left:
+                    </label>
+                    <div>
+                      {(timeLeft > 0) ? (
+                        <strong>{getDateDiffText(getUnixTimestamp(), Number(endTime) + 31 * 24 * 60 * 60)}</strong>
+                      ) : (
+                        <strong>{`Freeze time is up. You can take the bank`}</strong>
+                      )}
+                    </div>
+                  </div>
+                  <div className={styles.infoRow}>
+                    <label>
+                      <div className={styles.helpTooltip}>
+                        <span>?</span>
+                        <div>{`How much is in the lottery bank now`}</div>
+                      </div>
+                      Lottery bank:
+                    </label>
+                    <div>
+                      <div>
+                        <span>{balanceInfo.normalized}</span><strong>{storageData.tokenInfo.symbol}</strong>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.actionsRow}>
+                    <SwitchNetworkAndCall
+                      chainId={storageData.chainId}
+                      onClick={doBankWithdraw}
+                      disabled={isBankWithdraw || (timeLeft > 0) || (lotteryStatus != "3")}
+                      icon="money-bill-transfer"
+                      action="Withdraw bank"
+                      className={styles.adminButton}
+                    >
+                      {isBankWithdraw ? `Withdrawing lottery bank` : `Withdraw lottery bank`}
+                    </SwitchNetworkAndCall>
                   </div>
                 </div>
-                <div className={styles.infoRow}>
-                  <label>
-                    <div className={styles.helpTooltip}>
-                      <span>?</span>
-                      <div>{`How much time has passed since the last round closed`}</div>
-                    </div>
-                    Time left:
-                  </label>
-                  <div>
-                    Time
-                  </div>
-                </div>
-                <div className={styles.infoRow}>
-                  <label>
-                    <div className={styles.helpTooltip}>
-                      <span>?</span>
-                      <div>{`How much is in the lottery bank now`}</div>
-                    </div>
-                    Lottery bank:
-                  </label>
-                  <div>
-                    000 WENUS
-                  </div>
-                </div>
-                <div className={styles.actionsRow}>
-                  <SwitchNetworkAndCall
-                    chainId={storageData.chainId}
-                    onClick={doBankWithdraw}
-                    disabled={isBankWithdraw}
-                    icon="money-bill-transfer"
-                    action="Withdraw bank"
-                    className={styles.adminButton}
-                  >
-                    {isBankWithdraw ? `Withdrawing lottery bank` : `Withdraw lottery bank`}
-                  </SwitchNetworkAndCall>
-                </div>
-              </div>
+              ) : (
+                <div>Fetching info...</div>
+              )}
             </div>
           </div>
         </>
