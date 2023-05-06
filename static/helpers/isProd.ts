68cd7f78e226445bc1030339dfe7bb0b6f5e6ce5
shendel <im_shendel@inbox.ru>
2023-05-06 19:14:17 +0300

    cf pages - use dev blockchain

--- static/helpers/isProd.ts
+++ static/helpers/isProd.ts
@@ -11,6 +11,9 @@ const devDomains = [
 const isProd = () => {
   if (typeof window === 'undefined') return ''
   const curDomain = window.location.hostname || document.location.host || ''
+  // CF Pages - Use dev
+  if (curDomain.substr(-10,10) == '.pages.dev') return false
+  
   return devDomains.indexOf(curDomain) === -1
 }
 
