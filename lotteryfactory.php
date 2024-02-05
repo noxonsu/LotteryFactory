82848a00291954727590a68c074c6622c207fa49
Noxon <2914674+noxonsu@users.noreply.github.com>
2024-02-05 19:23:19 +0000

    Add .vscode to .gitignore and fix URL in lotteryfactory.php

--- lotteryfactory.php
+++ lotteryfactory.php
@@ -7,14 +7,24 @@ Text Domain: lotteryfactory
 Domain Path: /lang
 Version: 1.2.2
  */
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 /* Define Plugin Constants */
 defined( 'ABSPATH' ) || exit;
 define( 'LOTTERYFACTORY_TEMPLATE_DIR', __DIR__ . DIRECTORY_SEPARATOR . 'templates' );
 define( 'LOTTERYFACTORY_BASE_DIR', __DIR__ );
 define( 'LOTTERYFACTORY_BASE_FILE', __FILE__ );
 define( 'LOTTERYFACTORY_PATH', plugin_dir_path( __FILE__ ) );
-define( 'LOTTERYFACTORY_URL', plugin_dir_url( __FILE__ ) );
+
+$plugin_url = plugins_url('', __FILE__);
+
+// Check if the site is accessed via HTTPS and adjust the URL if necessary
+if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+    $plugin_url = str_replace('http://', 'https://', $plugin_url)."/";
+}
+echo $plugin_url;
+die(); 
+define( 'LOTTERYFACTORY_URL', $plugin_ur);
 define( 'LOTTERYFACTORY_VER', '1.2.2');
 
 /**
